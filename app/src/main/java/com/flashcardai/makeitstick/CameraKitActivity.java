ad693cb5be6d841b04fe6f337f3a11f2ea694369
Dylan Yung <YungDexiong@hotmail.com>
2018-06-07 19:32:49 -0700

    camerakit added

--- app/src/main/java/com/flashcardai/makeitstick/CameraKitActivity.java
+++ app/src/main/java/com/flashcardai/makeitstick/CameraKitActivity.java
@@ -4,11 +4,48 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.Toast;
 
+import com.wonderkiln.camerakit.CameraKitError;
+import com.wonderkiln.camerakit.CameraKitEvent;
+import com.wonderkiln.camerakit.CameraKitEventListener;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraKitVideo;
 import com.wonderkiln.camerakit.CameraView;
 
 public class CameraKitActivity extends AppCompatActivity {
     CameraView cameraView;
+    boolean canTakePicture;
+    private CameraKitEventListener cameraListener = new CameraKitEventListener() {
+        @Override
+        public void onEvent(CameraKitEvent cameraKitEvent) {
+            switch (cameraKitEvent.getType()) {
+                case CameraKitEvent.TYPE_CAMERA_OPEN:
+                    canTakePicture = true;
+                    break;
+
+                case CameraKitEvent.TYPE_CAMERA_CLOSE:
+                    canTakePicture = false;
+                    break;
+            }
+        }
+
+        @Override
+        public void onError(CameraKitError cameraKitError) {
+        }
+
+        @Override
+        public void onImage(CameraKitImage cameraKitImage) {
+            byte[] picture = cameraKitImage.getJpeg(); // will return byte[]
+            Bitmap result = BitmapFactory.decodeByteArray(picture, 0, picture.length);
+        }
+
+        @Override
+        public void onVideo(CameraKitVideo cameraKitVideo) {
+            return;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +53,7 @@ public class CameraKitActivity extends AppCompatActivity {
         setContentView(R.layout.activity_camera_kit);
         cameraView = findViewById(R.id.camera);
 
-        cameraView.captureImage();
+        cameraView.addCameraKitListener(cameraListener);
     }
 
     @Override
